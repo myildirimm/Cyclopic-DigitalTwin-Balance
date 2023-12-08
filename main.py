510248b2158f156283816905d03061e2ea0826e7
myildirimm <my00499@surrey.ac.uk>
2023-12-08 12:15:26 +0000

    not working
    not working

--- main.py
+++ main.py
@@ -76,185 +76,206 @@ agent = RLAgent()
 # the car class for the DQ
 class Cyclopic:
 
-        # Handle events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                rim_angle_1 += rotation_speed_1
-            elif event.key == pygame.K_RIGHT:
-                rim_angle_1 -= rotation_speed_1
-            elif event.key == pygame.K_a:
-                rim_angle_2 += rotation_speed_2
-            elif event.key == pygame.K_d:
-                rim_angle_2 -= rotation_speed_2
+    def __init__(self):
+        # Initialize environment state
+        self.rim_angle_1 = 0
+        self.rim_angle_2 = 0
 
-
-        # Update environment state
-        state = {
-            'acceleration': acceleration,
-            'rim_angle_1': rim_angle_1,
-            'rim_angle_2': rim_angle_2,
-            'resultant_angle': calculate_resultant_angle()  # Placeholder function
-        }
-
-
-        # Get action from RL agent
-        action = agent.choose_action(state)
-
-        # Apply the action to the rims (placeholder logic)
+    def update(self, action):
+        # Apply action to the environment and update the state
         # action could be a tuple like (change_in_rim_angle_1, change_in_rim_angle_2)
-        rim_angle_1 += action[0]
-        rim_angle_2 += action[1]
+        self.rim_angle_1 += action[0]
+        self.rim_angle_2 += action[1]
 
-        # Calculate reward (placeholder logic)
-        reward = calculate_reward(state)  # Function to calculate reward based on the state
-
-        next_state, reward, done = env.step(action)
-        agent.learn(state, action, reward, next_state)
-
-        # Fill the screen
-        screen.fill((255, 255, 255))
+    def get_state(self):
+        # Return the current state of the environment
+        state = {
+            'acceleration': acceleration,  # Update this as needed
+            'rim_angle_1': self.rim_angle_1,
+            'rim_angle_2': self.rim_angle_2,
+            # ... other state information ...
+        }
+        return state
     
-        shaft_base_pos_1 = (tire_pos_1[0] - (math.cos(math.radians((rim_angle_1))) * rim_radius)+10, tire_pos_1[1] + (math.sin(math.radians((rim_angle_1))) * rim_radius))
-        shaft_base_pos_2 = (tire_pos_2[0] - (math.cos(math.radians((rim_angle_2))) * rim_radius)+10, tire_pos_2[1] + (math.sin(math.radians((rim_angle_2))) * rim_radius))
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            rim_angle_1 += rotation_speed_1
+        elif event.key == pygame.K_RIGHT:
+            rim_angle_1 -= rotation_speed_1
+        elif event.key == pygame.K_a:
+            rim_angle_2 += rotation_speed_2
+        elif event.key == pygame.K_d:
+            rim_angle_2 -= rotation_speed_2
 
-        shaft_end_pos_1 = (shaft_base_pos_1[0],shaft_base_pos_1[1]- shaft_length)
-        shaft_end_pos_2 = (shaft_base_pos_2[0],shaft_base_pos_2[1]- shaft_length)
 
-        # Draw the shaft
-        shaft_rect_1 = pygame.Rect(shaft_base_pos_1[0] - shaft_width // 2,
-                                shaft_base_pos_1[1] - shaft_length,
-                                shaft_width,
-                                shaft_length)
-        shaft_rect_2 = pygame.Rect(shaft_base_pos_2[0] - shaft_width // 2,
-                                shaft_base_pos_2[1] - shaft_length,
-                                shaft_width,
-                                shaft_length)
-        pygame.draw.rect(screen, METALLIC, shaft_rect_1)
-        pygame.draw.rect(screen, METALLIC, shaft_rect_2)
-        
-        base_rect = pygame.Rect(shaft_end_pos_1[0],
-                                shaft_end_pos_1[1] - base_width,
-                                base_length,
-                                base_width )
-        
-        # Blit the tire image onto the screen at the tire positions
-        tire_rect_1 = tire_image.get_rect(center=tire_pos_1)
-        tire_rect_2 = tire_image.get_rect(center=tire_pos_2)
-        screen.blit(tire_image, tire_rect_1.topleft)
-        screen.blit(tire_image, tire_rect_2.topleft)
+    # Update environment state
+    state = {
+        'acceleration': acceleration,
+        'rim_angle_1': rim_angle_1,
+        'rim_angle_2': rim_angle_2,
+        'resultant_angle': calculate_resultant_angle()  # Placeholder function
+    }
 
-        # Create a surface for the rim to rotate
-        rim_surface_1 = pygame.Surface((2 * rim_radius, 2 * rim_radius), pygame.SRCALPHA)
-        pygame.draw.circle(rim_surface_1, GREY, (rim_radius, rim_radius), rim_radius, rim_thickness)
 
-        # Rotate the rim
-        rotated_rim_1 = pygame.transform.rotate(rim_image, rim_angle_1)
-        # Get the new rect for the rotated rim to blit at the correct position
-        rotated_rim_rect_1 = rotated_rim_1.get_rect(center=tire_pos_1)
+    # Get action from RL agent
+    action = agent.choose_action(state)
 
-        rotated_rim_2 = pygame.transform.rotate(rim_image, rim_angle_2)
-        # Get the new rect for the rotated rim to blit at the correct position
-        rotated_rim_rect_2 = rotated_rim_2.get_rect(center=tire_pos_2)
-        # Blit the rotated rim onto the screen
-        screen.blit(rotated_rim_1, rotated_rim_rect_1.topleft)
-        screen.blit(rotated_rim_2, rotated_rim_rect_2.topleft)
+    # Apply the action to the rims (placeholder logic)
+    # action could be a tuple like (change_in_rim_angle_1, change_in_rim_angle_2)
+    rim_angle_1 += action[0]
+    rim_angle_2 += action[1]
 
-        pygame.draw.line(screen,METALLIC, (shaft_end_pos_1[0]- shaft_width/2,
-                                shaft_end_pos_1[1]-base_width/2 ), (shaft_base_pos_2[0]+shaft_width/2,shaft_base_pos_2[1]- shaft_length-base_width/2),20)
-        
-        # Calculate the angle of the base plate
-        base_plate_angle = calculate_angle(shaft_end_pos_1, shaft_end_pos_2)
+    # Calculate reward (placeholder logic)
+    reward = calculate_reward(state)  # Function to calculate reward based on the state
 
-        # Create a surface for the box
-        box_surface = pygame.Surface((box_width, box_height), pygame.SRCALPHA)  # Use SRCALPHA for transparency
-        box_surface.fill(YELLOW)  # Fill the surface with the box color
+    next_state, reward, done = env.step(action)
+    agent.learn(state, action, reward, next_state)
 
-        # Rotate the box surface
-        rotated_box = pygame.transform.rotate(box_surface, - base_plate_angle)  # Negative angle for correct rotation direction
+    # Fill the screen
+    screen.fill((255, 255, 255))
 
-        # Calculate the new position for the rotated box
-        box_center_x = random_x # (shaft_end_pos_1[0] + shaft_end_pos_2[0]) / 2
+    shaft_base_pos_1 = (tire_pos_1[0] - (math.cos(math.radians((rim_angle_1))) * rim_radius)+10, tire_pos_1[1] + (math.sin(math.radians((rim_angle_1))) * rim_radius))
+    shaft_base_pos_2 = (tire_pos_2[0] - (math.cos(math.radians((rim_angle_2))) * rim_radius)+10, tire_pos_2[1] + (math.sin(math.radians((rim_angle_2))) * rim_radius))
 
-        # Find the bottom of the rotated box
-        rotated_box_rect = rotated_box.get_rect(center=(box_center_x, shaft_end_pos_1[1] - base_width))
-        box_bottom_y = rotated_box_rect.bottom
+    shaft_end_pos_1 = (shaft_base_pos_1[0],shaft_base_pos_1[1]- shaft_length)
+    shaft_end_pos_2 = (shaft_base_pos_2[0],shaft_base_pos_2[1]- shaft_length)
 
-        # Adjust y-position so the bottom of the box aligns with the top of the platform
-        box_center_y = shaft_end_pos_1[1] - base_width - (rotated_box.get_height() / 2)
+    # Draw the shaft
+    shaft_rect_1 = pygame.Rect(shaft_base_pos_1[0] - shaft_width // 2,
+                            shaft_base_pos_1[1] - shaft_length,
+                            shaft_width,
+                            shaft_length)
+    shaft_rect_2 = pygame.Rect(shaft_base_pos_2[0] - shaft_width // 2,
+                            shaft_base_pos_2[1] - shaft_length,
+                            shaft_width,
+                            shaft_length)
+    pygame.draw.rect(screen, METALLIC, shaft_rect_1)
+    pygame.draw.rect(screen, METALLIC, shaft_rect_2)
+    
+    base_rect = pygame.Rect(shaft_end_pos_1[0],
+                            shaft_end_pos_1[1] - base_width,
+                            base_length,
+                            base_width )
+    
+    # Blit the tire image onto the screen at the tire positions
+    tire_rect_1 = tire_image.get_rect(center=tire_pos_1)
+    tire_rect_2 = tire_image.get_rect(center=tire_pos_2)
+    screen.blit(tire_image, tire_rect_1.topleft)
+    screen.blit(tire_image, tire_rect_2.topleft)
 
-        # Update the rect for proper positioning
-        rotated_box_rect = rotated_box.get_rect(center=(box_center_x, box_center_y))
+    # Create a surface for the rim to rotate
+    rim_surface_1 = pygame.Surface((2 * rim_radius, 2 * rim_radius), pygame.SRCALPHA)
+    pygame.draw.circle(rim_surface_1, GREY, (rim_radius, rim_radius), rim_radius, rim_thickness)
 
-        rotated_box_rect = rotated_box.get_rect(center=(box_center_x, box_center_y))
+    # Rotate the rim
+    rotated_rim_1 = pygame.transform.rotate(rim_image, rim_angle_1)
+    # Get the new rect for the rotated rim to blit at the correct position
+    rotated_rim_rect_1 = rotated_rim_1.get_rect(center=tire_pos_1)
 
-        # Blit the rotated box onto the screen
-        screen.blit(rotated_box, rotated_box_rect.topleft)   
-        box_position_y = shaft_end_pos_1[1] - box_height
+    rotated_rim_2 = pygame.transform.rotate(rim_image, rim_angle_2)
+    # Get the new rect for the rotated rim to blit at the correct position
+    rotated_rim_rect_2 = rotated_rim_2.get_rect(center=tire_pos_2)
+    # Blit the rotated rim onto the screen
+    screen.blit(rotated_rim_1, rotated_rim_rect_1.topleft)
+    screen.blit(rotated_rim_2, rotated_rim_rect_2.topleft)
 
-        # Calculate the angle difference between the two wheels
-        angle_difference = abs(rim_angle_1 - rim_angle_2)
-        # Determine the direction of the CoG shift
-        cog_shift_direction = 1 if rim_angle_1 < rim_angle_2 else -1
-        # Calculate the CoG shift
-        cog_shift = angle_difference * cog_shift_per_degree * cog_shift_direction
+    pygame.draw.line(screen,METALLIC, (shaft_end_pos_1[0]- shaft_width/2,
+                            shaft_end_pos_1[1]-base_width/2 ), (shaft_base_pos_2[0]+shaft_width/2,shaft_base_pos_2[1]- shaft_length-base_width/2),20)
+    
+    # Calculate the angle of the base plate
+    base_plate_angle = calculate_angle(shaft_end_pos_1, shaft_end_pos_2)
 
-        cog_arrow_base_x = box_center_x  # CoG arrow base is at the mid-point of the box
-        cog_arrow_base_y = box_position_y + box_height // 2  # CoG arrow base is at the vertical center of the box
+    # Create a surface for the box
+    box_surface = pygame.Surface((box_width, box_height), pygame.SRCALPHA)  # Use SRCALPHA for transparency
+    box_surface.fill(YELLOW)  # Fill the surface with the box color
 
-        # Calculate the CoG arrow tip position
-        cog_arrow_tip_x = cog_arrow_base_x + cog_shift
-        cog_arrow_tip_y = cog_arrow_base_y + cog_length  # The arrow points downwards
+    # Rotate the box surface
+    rotated_box = pygame.transform.rotate(box_surface, - base_plate_angle)  # Negative angle for correct rotation direction
 
-        # Draw the CoG arrow
-        pygame.draw.line(screen, RED, (cog_arrow_base_x, cog_arrow_base_y), (cog_arrow_tip_x, cog_arrow_tip_y), 5)
+    # Calculate the new position for the rotated box
+    box_center_x = random_x # (shaft_end_pos_1[0] + shaft_end_pos_2[0]) / 2
 
-        # Draw the CoG arrowhead
-        cog_arrow_tip = (cog_arrow_tip_x, cog_arrow_tip_y)
-        cog_arrow_left = (cog_arrow_tip_x - arrowhead_size, cog_arrow_tip_y - arrowhead_size)
-        cog_arrow_right = (cog_arrow_tip_x + arrowhead_size, cog_arrow_tip_y - arrowhead_size)
-        pygame.draw.polygon(screen, RED, [cog_arrow_tip, cog_arrow_left, cog_arrow_right])
+    # Find the bottom of the rotated box
+    rotated_box_rect = rotated_box.get_rect(center=(box_center_x, shaft_end_pos_1[1] - base_width))
+    box_bottom_y = rotated_box_rect.bottom
 
-        # Acceleration Arrow
-        # Determine the direction of movement
-        direction = -1 if acceleration < 0 else 1  # Left for negative, right for positive
+    # Adjust y-position so the bottom of the box aligns with the top of the platform
+    box_center_y = shaft_end_pos_1[1] - base_width - (rotated_box.get_height() / 2)
 
-        # Calculate the acceleration arrow length
-        accel_arrow_length = abs(acceleration) * arrow_length_per_unit
+    # Update the rect for proper positioning
+    rotated_box_rect = rotated_box.get_rect(center=(box_center_x, box_center_y))
 
-        # Set the start point of the acceleration arrow (same as the CoG arrow base)
-        accel_arrow_start_x = cog_arrow_base_x
-        accel_arrow_start_y = cog_arrow_base_y
+    rotated_box_rect = rotated_box.get_rect(center=(box_center_x, box_center_y))
 
-        # Calculate the end point of the acceleration arrow
-        accel_arrow_end_x = accel_arrow_start_x + (direction * accel_arrow_length)
-        accel_arrow_end_y = accel_arrow_start_y
+    # Blit the rotated box onto the screen
+    screen.blit(rotated_box, rotated_box_rect.topleft)   
+    box_position_y = shaft_end_pos_1[1] - box_height
 
-        # Draw the acceleration arrow
-        pygame.draw.line(screen, RED, (accel_arrow_start_x, accel_arrow_start_y), (accel_arrow_end_x, accel_arrow_end_y), arrow_width)
+    # Calculate the angle difference between the two wheels
+    angle_difference = abs(rim_angle_1 - rim_angle_2)
+    # Determine the direction of the CoG shift
+    cog_shift_direction = 1 if rim_angle_1 < rim_angle_2 else -1
+    # Calculate the CoG shift
+    cog_shift = angle_difference * cog_shift_per_degree * cog_shift_direction
 
-        # Draw the acceleration arrowhead
-        accel_arrowhead_left = (accel_arrow_end_x - direction * arrowhead_size, accel_arrow_end_y - arrowhead_size)
-        accel_arrowhead_right = (accel_arrow_end_x - direction * arrowhead_size, accel_arrow_end_y + arrowhead_size)
-        pygame.draw.polygon(screen, RED, [(accel_arrow_end_x, accel_arrow_end_y), accel_arrowhead_left, accel_arrowhead_right])
+    cog_arrow_base_x = box_center_x  # CoG arrow base is at the mid-point of the box
+    cog_arrow_base_y = box_position_y + box_height // 2  # CoG arrow base is at the vertical center of the box
 
-        # Resultant Arrow
-        # Calculate the resultant arrow's end point
-        resultant_arrow_end_x = cog_arrow_base_x + (cog_arrow_tip_x - cog_arrow_base_x) + (accel_arrow_end_x - cog_arrow_base_x)
-        resultant_arrow_end_y = cog_arrow_base_y + (cog_arrow_tip_y - cog_arrow_base_y) + (accel_arrow_end_y - cog_arrow_base_y)
+    # Calculate the CoG arrow tip position
+    cog_arrow_tip_x = cog_arrow_base_x + cog_shift
+    cog_arrow_tip_y = cog_arrow_base_y + cog_length  # The arrow points downwards
 
-        # Draw the resultant arrow
-        pygame.draw.line(screen, BLUE, (cog_arrow_base_x, cog_arrow_base_y), (resultant_arrow_end_x, resultant_arrow_end_y), resultant_arrow_width)
+    # Draw the CoG arrow
+    pygame.draw.line(screen, RED, (cog_arrow_base_x, cog_arrow_base_y), (cog_arrow_tip_x, cog_arrow_tip_y), 5)
 
-        # Draw the resultant arrowhead
-        resultant_arrowhead_left = (resultant_arrow_end_x - resultant_arrowhead_size, resultant_arrow_end_y - resultant_arrowhead_size)
-        resultant_arrowhead_right = (resultant_arrow_end_x - resultant_arrowhead_size, resultant_arrow_end_y + resultant_arrowhead_size)
-        pygame.draw.polygon(screen, BLUE, [(resultant_arrow_end_x, resultant_arrow_end_y), resultant_arrowhead_left, resultant_arrowhead_right])
+    # Draw the CoG arrowhead
+    cog_arrow_tip = (cog_arrow_tip_x, cog_arrow_tip_y)
+    cog_arrow_left = (cog_arrow_tip_x - arrowhead_size, cog_arrow_tip_y - arrowhead_size)
+    cog_arrow_right = (cog_arrow_tip_x + arrowhead_size, cog_arrow_tip_y - arrowhead_size)
+    pygame.draw.polygon(screen, RED, [cog_arrow_tip, cog_arrow_left, cog_arrow_right])
 
-        pygame.display.flip()
+    # Acceleration Arrow
+    # Determine the direction of movement
+    direction = -1 if acceleration < 0 else 1  # Left for negative, right for positive
 
-        # Cap the frame rate
-        clock.tick(10)
+    # Calculate the acceleration arrow length
+    accel_arrow_length = abs(acceleration) * arrow_length_per_unit
+
+    # Set the start point of the acceleration arrow (same as the CoG arrow base)
+    accel_arrow_start_x = cog_arrow_base_x
+    accel_arrow_start_y = cog_arrow_base_y
+
+    # Calculate the end point of the acceleration arrow
+    accel_arrow_end_x = accel_arrow_start_x + (direction * accel_arrow_length)
+    accel_arrow_end_y = accel_arrow_start_y
+
+    # Draw the acceleration arrow
+    pygame.draw.line(screen, RED, (accel_arrow_start_x, accel_arrow_start_y), (accel_arrow_end_x, accel_arrow_end_y), arrow_width)
+
+    # Draw the acceleration arrowhead
+    accel_arrowhead_left = (accel_arrow_end_x - direction * arrowhead_size, accel_arrow_end_y - arrowhead_size)
+    accel_arrowhead_right = (accel_arrow_end_x - direction * arrowhead_size, accel_arrow_end_y + arrowhead_size)
+    pygame.draw.polygon(screen, RED, [(accel_arrow_end_x, accel_arrow_end_y), accel_arrowhead_left, accel_arrowhead_right])
+
+    # Resultant Arrow
+    # Calculate the resultant arrow's end point
+    resultant_arrow_end_x = cog_arrow_base_x + (cog_arrow_tip_x - cog_arrow_base_x) + (accel_arrow_end_x - cog_arrow_base_x)
+    resultant_arrow_end_y = cog_arrow_base_y + (cog_arrow_tip_y - cog_arrow_base_y) + (accel_arrow_end_y - cog_arrow_base_y)
+
+    # Draw the resultant arrow
+    pygame.draw.line(screen, BLUE, (cog_arrow_base_x, cog_arrow_base_y), (resultant_arrow_end_x, resultant_arrow_end_y), resultant_arrow_width)
+
+    # Draw the resultant arrowhead
+    resultant_arrowhead_left = (resultant_arrow_end_x - resultant_arrowhead_size, resultant_arrow_end_y - resultant_arrowhead_size)
+    resultant_arrowhead_right = (resultant_arrow_end_x - resultant_arrowhead_size, resultant_arrow_end_y + resultant_arrowhead_size)
+    pygame.draw.polygon(screen, BLUE, [(resultant_arrow_end_x, resultant_arrow_end_y), resultant_arrowhead_left, resultant_arrowhead_right])
+
+    pygame.display.flip()
+
+    # Cap the frame rate
+    clock.tick(10)
 
